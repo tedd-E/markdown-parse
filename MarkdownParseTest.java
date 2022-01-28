77f04bc1422db83cc27f64fe54ef03121d048670
tedd-E <62875128+tedd-E@users.noreply.github.com>
2022-01-27 16:26:26 -0800

    add second test case

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,9 +1,21 @@
 import static org.junit.Assert.*;
 import org.junit.*;
+import java.io.*;
+import java.nio.file.*;
+import java.util.*;
 
 public class MarkdownParseTest {
     @Test
     public void addition() {
         assertEquals(2, 1 + 1);
+    }
+
+    @Test
+    public void testNewFile() throws IOException {
+        Path filename = Path.of("test-file.md");
+        String contents = Files.readString(filename);
+        ArrayList<String> links = MarkdownParse.getLinks(contents);
+        assertEquals("Test the links of test-file.md", List.of("https://something.com", "some-page.html"), links);
+
     }
 }
